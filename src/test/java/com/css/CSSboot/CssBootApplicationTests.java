feb9b0662d34462f50df3685bc211c276360f132
toto <ahmadou@css.sn>
2024-02-14 12:18:11 +0000

    initial commit

---
+++ src/test/java/com/css/CSSboot/CssBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.css.CSSboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
